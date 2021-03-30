928291f3a24bceeca7cc5bfe1033295428a05689
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2021-03-30 09:39:45 +0300

    Add files via upload

---
+++ UnitTest3.1A/UnitTest3.1A.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project3.1/Triangle.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest31A
+{
+	TEST_CLASS(UnitTest31A)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Triangle a(0, 0, 0);
+			bool k = a.A();
+			Assert::AreEqual(k, true);
+		}
+	};
+}
